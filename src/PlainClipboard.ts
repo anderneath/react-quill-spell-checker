c86c8d77a93fcbab395de3b11a228346c21a1f6f
Flavio Rocha <flaviorocha@estantemagica.com.br>
2023-10-16 11:00:58 -0300

    fix: update regex

--- src/PlainClipboard.ts
+++ src/PlainClipboard.ts
@@ -3,8 +3,7 @@ const Clipboard = Quill.import("modules/clipboard")
 const Delta = Quill.import("delta")
 
 export const specialCharacters =
-  /<|>|\u00a9|\u00ae|[\u2000-\u2013]|[\u2015-\u2017]|[\u2020-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]|[\u1d00-\u1d7f]|[\u0250-\u02af]|[\u0100-\u017f]|\ud835[\udc00-\udfff]/g
-
+  /<|>|\u00a9|\u00ae|[\u2000-\u200f]|[\u2016-\u2017]|[\u2020-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]|[\u1d00-\u1d7f]|[\u0250-\u02af]|[\u0100-\u017f]|\ud835[\udc00-\udfff]/g
 export default class PlainClipboard extends Clipboard {
   onPaste(e: any) {
     e.preventDefault()
